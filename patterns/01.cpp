7ae8ae0abd79676a80e860ac624dd18e93883fdc
nydycodes <149265131+nydycodes@users.noreply.github.com>
2024-06-26 12:53:53 +0530

    Update 01.cpp

--- patterns/01.cpp
+++ patterns/01.cpp
@@ -12,8 +12,8 @@ using namespace std;
 int main(){
     int n;
     cin>>n;
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
             cout<<"*"<<" ";
         }
         cout<<endl;
