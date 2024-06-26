2908010ddc752b954e7044969fec7db12197a80b
nydycodes <149265131+nydycodes@users.noreply.github.com>
2024-06-26 10:24:06 +0530

    Create 3.cpp

---
+++ patterns/3.cpp
@@ -0,0 +1,24 @@
+/*
+
+1 
+1 2 
+1 2 3 
+1 2 3 4 
+1 2 3 4 5 
+
+*/
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n,count;
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        count = 1;
+        for(int j=1;j<=i;j++){
+            cout<<count++<<" ";
+        }
+        cout<<endl;
+    }
+}
