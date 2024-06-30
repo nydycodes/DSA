dcded04a727f35c11123aad7a404ee9734cc4542
nydycodes <149265131+nydycodes@users.noreply.github.com>
2024-06-30 17:26:19 +0530

    Create fibonacci.cpp

---
+++ recursion/fibonacci.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int fibo(int n){
+    if(n==0){
+        return 0;
+    }
+    else if(n==1){
+        return 1;
+    }
+    else{
+        return fibo(n-1)+fibo(n-2);
+    }
+}
+
+int main(){
+    int n;
+    cout<<"enter the value of n: ";
+    cin>>n;
+    
+    for(int i=0;i<=n;i++){
+        cout<<fibo(i)<<" ";
+    }
+    cout<<endl;
+}
