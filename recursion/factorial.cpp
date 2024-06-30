f73df490ce2ab830b8ec9f3b82e7891c0bbbbb14
nydycodes <149265131+nydycodes@users.noreply.github.com>
2024-06-30 13:04:46 +0530

    Create factorial.cpp

---
+++ recursion/factorial.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+int fact(int n){
+    if(n==0||n==1){
+        return 1;
+    }
+    return n*fact(n-1);
+}
+int main(){
+    int n;
+    cout<<"enter the value of n: ";
+    cin>>n;
+    
+    cout<<"factorial: "<<fact(n)<<endl;
+}#include<iostream>
+using namespace std;
+
+int fact(int n){
+    if(n==0||n==1){
+        return 1;
+    }
+    return n*fact(n-1);
+}
+int main(){
+    int n;
+    cout<<"enter the value of n: ";
+    cin>>n;
+    
+    cout<<"factorial: "<<fact(n)<<endl;
+}
